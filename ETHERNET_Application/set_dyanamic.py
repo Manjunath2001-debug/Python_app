d0fd048f4be7b3d34068502a3e68cea31d25d76c
Manjunath2001-debug <badigermanju997@gmail.com>
2025-06-09 13:09:57 +0530

    Update set_dyanamic.py

--- ETHERNET_Application/set_dyanamic.py
+++ ETHERNET_Application/set_dyanamic.py
@@ -37,29 +37,3 @@ def set_dyanamic_ip(interface):
         print(f"Failed to configure DHCP using systemd: {e}")
 
 
-#-----------------------------------------------------------------------------------------------------------
-# import os
-
-# def set_dynamic_ip(interface):
-#     """
-#     Configures DHCP (dynamic IP) for a network interface in /etc/network/interfaces.
-
-#     :param interface: Network interface name (e.g., 'eth0')
-#     """
-
-#     dhcp_config = f"""auto {interface}
-#     iface {interface} inet dhcp
-#     """
-
-#     try:
-#         os.system("sudo cp /etc/network/interfaces /etc/network/interfaces.bak")
-
-#         with open("/etc/network/interfaces", "w") as f:
-#             f.write(dhcp_config)
-
-#         print("DHCP configuration written successfully.")
-
-#         os.system("sudo systemctl restart networking")
-#         print("Networking service restarted.")
-#     except Exception as e:
-#         print(f"Failed to configure DHCP: {e}")
